47b78e1a10c7c7677aae45f45b03347304be99b2
Michaela Novotna <michaela.novotna.8@seznam.cz>
2022-04-10 17:27:57 +0200

    final-assign

--- assignments/module5-solution/js/script.js
+++ assignments/module5-solution/js/script.js
@@ -89,8 +89,7 @@ $ajaxUtils.sendGetRequest(
 // *** finish **
 
 
-// Builds HTML for the home page based on categories array
-// returned from the server.
+// Builds HTML for the home page based on categories array returned from the server.
 function buildAndShowHomeHTML (categories) {
 
   // Load home snippet page
@@ -98,9 +97,7 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
-      // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
-      // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
+      // Pay attention to what type of data that function returns vs what the chosenCategoryShortName variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -111,15 +108,10 @@ function buildAndShowHomeHTML (categories) {
       // being passed into the $dc.loadMenuItems function. Think about what that argument needs
       // to look like. For example, a valid call would look something like this:
       // $dc.loadMenuItems('L')
-      // Hint: you need to surround the chosen category short name with something before inserting
-      // it into the home html snippet.
-      //
-      // var homeHtmlToInsertIntoMainPage = ....
+      // Hint: you need to surround the chosen category short name with something before inserting it into the home html snippet.
       var homeHtmlToInsertIntoMainPage = insertProperty (homeHtml, 'randomCategoryShortName', '\'' + chosenCategoryShortName + '\'');
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
-      // Use the existing insertHtml function for that purpose. Look through this code for an example
-      // of how to do that.
-      // ....
+      // Use the existing insertHtml function for that purpose. Look through this code for an example of how to do that.
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
 
     },
